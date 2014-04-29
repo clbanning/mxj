696c5166bdb73fdeb8074740c2a85ca1b2b55843
clbanning <clbanning@gmail.com>
2014-04-29 15:25:02 -0500

    Warning re. bulk processing.

--- doc.go
+++ doc.go
@@ -9,6 +9,8 @@ Marshal/Unmarshal XML to/from JSON and map[string]interface{} values, and extrac
 mxj supplants the legacy x2j and j2x packages. If you want the old syntax, just uncomment the content of x2j.go
 or j2x.go, which are included as part of this package.
 
+Note: this library was designed for processing ad hoc anonymous messages.  Bulk processing large data sets may be much more efficiently performed using the encoding/xml or encoding/json packages from Go's standard library directly.
+
 SUMMARY
 
    type Map map[string]interface{}
