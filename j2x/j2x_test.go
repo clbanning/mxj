d2327b54d3d149aa11efc4dfcd4152406018e58b
clbanning <clbanning@gmail.com>
2014-07-30 09:59:39 -0500

    Cleanup j2x test
    Handling special case of parsing JSON list doc.

--- j2x/j2x_test.go
+++ j2x/j2x_test.go
@@ -36,38 +36,6 @@ func TestJsonToXml_1(t *testing.T) {
 	fmt.Println("xmloutput:", string(xmloutput))
 }
 
-func TestJsonToXml_1A(t *testing.T) {
-	// mimic a io.Reader
-	// Body := bytes.NewReader([]byte(`{"some-null-value":"", "a-non-null-value":"bar"}`))
-	Body := bytes.NewReader([]byte(`[{"some-null-value":"", "a-non-null-value":"bar"}]`))
-
-	//body, err := ioutil.ReadAll(req.Body)
-	body, err := ioutil.ReadAll(Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	fmt.Println(string(body))
-	//if err != nil {
-	//	t.Fatal(err)
-	//}
-
-	if body[0] == '[' {
-		body = body[1:len(body)-1]
-	}
-	var xmloutput []byte
-	//xmloutput, err = j2x.JsonToXml(body)
-	xmloutput, err = JsonToXml(body)
-
-	//log.Println(string(xmloutput))
-
-	if err != nil {
-		t.Fatal(err)
-		// log.Println(err)
-		// http.Error(rw, "Could not convert to xml", 400)
-	}
-	fmt.Println("xmloutput:", string(xmloutput))
-}
-
 func TestJsonToXml_2(t *testing.T) {
 	// mimic a io.Reader
 	Body := bytes.NewReader([]byte(`{"somekey":[{"value":"1st"},{"value":"2nd"}]}`))
