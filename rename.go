e1f204befd203c6e434b03fe398358f97871b221
Atnyashev Marsel <atn@marsel.name>
2015-04-13 16:47:30 +0500

    RenameKey renames a key

---
+++ rename.go
@@ -0,0 +1,35 @@
+package mxj
+
+import (
+	"errors"
+	"strings"
+)
+
+// RenameKey renames a key in a Map.
+// TODO(mrsln): It works only for nested maps. It doesn't work for cases when it buried in a list.
+func (mv Map) RenameKey(path string, newName string) error {
+	m := map[string]interface{}(mv)
+	return renameKey_(m, path, newName)
+}
+
+func renameKey_(m interface{}, path string, newName string) error {
+	keys := strings.Split(path, ".")
+
+	switch mValue := m.(type) {
+	case map[string]interface{}:
+		for key, value := range mValue {
+			if key == keys[0] {
+				if len(keys) == 1 {
+					// renaming
+					mValue[newName] = value
+					delete(mValue, key)
+					return nil
+				} else {
+					// keep looking for the full path to the key
+					return renameKey_(value, strings.Join(keys[1:], "."), newName)
+				}
+			}
+		}
+	}
+	return errors.New("RenameKey didn't find the path: " + path)
+}
