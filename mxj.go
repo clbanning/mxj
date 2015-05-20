1b54c66e0e3520434ae415c32e67bce2738ee61b
Charles Banning <clbanning@tamgroupinc.com>
2015-05-20 11:22:07 -0500

    Map printing w/o type info - add alpha sort

--- mxj.go
+++ mxj.go
@@ -7,6 +7,7 @@ package mxj
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 )
 
@@ -54,6 +55,10 @@ func (mv Map) StringIndent(offset ...int) string {
 	return writeMap(map[string]interface{}(mv), offset...)
 }
 
+func (mv Map) StringIndentNoTypeInfo(offset ...int) string {
+	return writeMapNoTypes(map[string]interface{}(mv), offset...)
+}
+
 // writeMap - dumps the map[string]interface{} for examination.
 //	'offset' is initial indentation count; typically: Write(m).
 func writeMap(m interface{}, offset ...int) string {
@@ -92,16 +97,103 @@ func writeMap(m interface{}, offset ...int) string {
 			s += writeMap(v, indent+1)
 		}
 	case map[string]interface{}:
+		list := make([][2]string, len(m.(map[string]interface{})))
+		var n int
 		for k, v := range m.(map[string]interface{}) {
+			list[n][0] = k
+			list[n][1] = writeMap(v, indent+1)
+			n++
+		}
+		sort.Sort(mapList(list))
+		for _, v := range list {
 			s += "\n"
 			for i := 0; i < indent; i++ {
 				s += "  "
 			}
-			s += k + " :" + writeMap(v, indent+1)
+			s += v[0] + " :" + v[1]
 		}
 	default:
 		// shouldn't ever be here ...
 		s += fmt.Sprintf("[unknown] %#v", m)
 	}
 	return s
+}
+
+// writeMapNoTypes - dumps the map[string]interface{} for examination.
+//	'offset' is initial indentation count; typically: Write(m).
+func writeMapNoTypes(m interface{}, offset ...int) string {
+	var indent int
+	if len(offset) == 1 {
+		indent = offset[0]
+	}
+
+	var s string
+	switch m.(type) {
+	case nil:
+		return "nil"
+	case string:
+		return m.(string)
+	case float64:
+		return strconv.FormatFloat(m.(float64), 'e', 2, 64)
+	case bool:
+		return strconv.FormatBool(m.(bool))
+	case []interface{}:
+		s += ""
+		for i, v := range m.([]interface{}) {
+			s += "\n"
+			for i := 0; i < indent; i++ {
+				s += "  "
+			}
+			s += "[" + strconv.FormatInt(int64(i), 10) + "]"
+			switch v.(type) {
+			case string, float64, bool:
+				s += "\n"
+			default:
+				// noop
+			}
+			for i := 0; i < indent; i++ {
+				s += "  "
+			}
+			s += writeMapNoTypes(v, indent+1)
+		}
+	case map[string]interface{}:
+		list := make([][2]string, len(m.(map[string]interface{})))
+		var n int
+		for k, v := range m.(map[string]interface{}) {
+			list[n][0] = k
+			list[n][1] = writeMapNoTypes(v, indent+1)
+			n++
+		}
+		sort.Sort(mapList(list))
+		for _, v := range list {
+			s += "\n"
+			for i := 0; i < indent; i++ {
+				s += "  "
+			}
+			s += v[0] + " :" + v[1]
+		}
+	default:
+		// shouldn't ever be here ...
+		s += fmt.Sprintf("[?] %#v", m)
+	}
+	return s
+}
+
+// ======================== utility ===============
+
+type mapList [][2]string
+
+func (ml mapList) Len() int {
+	return len(ml)
+}
+
+func (ml mapList) Swap(i, j int) {
+	ml[i], ml[j] = ml[j], ml[i]
+}
+
+func (ml mapList) Less(i, j int) bool {
+	if ml[i][0] > ml[j][0] {
+		return false
+	}
+	return true
 }
