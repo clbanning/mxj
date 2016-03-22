eeb9f12e174ff44be7b7be97c182c110dbc30861
Charles Banning <clbanning@tamgroupinc.com>
2016-03-22 11:02:25 -0500

    another example

---
+++ examples/gitissue2.go
@@ -0,0 +1,36 @@
+// https://github.com/clbanning/mxj/issues/17
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/clbanning/mxj"
+	"io"
+	"io/ioutil"
+)
+
+func main() {
+	b, err := ioutil.ReadFile("gitissue2.dat")
+	if err != nil {
+		fmt.Println("err:", err)
+		return
+	}
+	r := bytes.NewReader(b)
+	m := make(map[string]interface{})
+	for {
+		v, err := mxj.NewMapXmlSeqReader(r)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			if err != mxj.NoRoot {
+				// handle error
+			}
+		}
+		for key, val := range v {
+			m[key] = val
+		}
+	}
+	fmt.Printf("%v\n", m)
+}
