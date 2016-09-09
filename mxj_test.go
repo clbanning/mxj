1db1a88bacc9ce20f8c8e317fe58e3c655a014cd
Charles Banning <clbanning@tamgroupinc.com>
2016-09-09 15:19:15 -0600

    Cleanup and simplify mv.StringIndent() and mv.StringIndentNoTypeInfo()

--- mxj_test.go
+++ mxj_test.go
@@ -18,8 +18,12 @@ func TestMap(t *testing.T) {
 	v["listkey"] = interface{}(vv)
 	m["newkey"] = interface{}(v)
 
-	fmt.Println("TestMap, m:", m)
-	fmt.Println("TestMap, StringIndent:", m.StringIndent())
+	fmt.Println("TestMap, m:")
+	fmt.Printf("%#v\n", m)
+	fmt.Println("TestMap, StringIndent -")
+	fmt.Println(m.StringIndent())
+	fmt.Println("TestMap, StringIndent NoTypeInfo -")
+	fmt.Println(m.StringIndentNoTypeInfo())
 
 	o := interface{}(m.Old())
 	switch o.(type) {
@@ -30,9 +34,13 @@ func TestMap(t *testing.T) {
 	}
 
 	m, _ = NewMapXml([]byte(`<doc><tag><sub_tag1>Hello</sub_tag1><sub_tag2>World</sub_tag2></tag></doc>`))
-	fmt.Println("TestMap, m_fromXML:", m)
-	fmt.Println("TestMap, StringIndent:", m.StringIndent())
+	fmt.Println("TestMap, m_fromXML:")
+	fmt.Printf("%#v\n", m)
+	fmt.Println("TestMap, StringIndent -")
+	fmt.Println( m.StringIndent())
+	fmt.Println("TestMap, StringIndent NoTypeInfo -")
+	fmt.Println( m.StringIndentNoTypeInfo())
 
 	mm, _ := m.Copy()
-	fmt.Println("TestMap, m.Copy():", mm)
+	fmt.Println("TestMap, m.Copy() -\n", mm)
 }
