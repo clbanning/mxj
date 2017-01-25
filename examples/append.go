e375e2b430ccd46ea15253a78483cebe94963ae6
Charles Banning <clbanning@tamgroupinc.com>
2017-01-25 08:41:20 -0700

    improve example for append

--- examples/append.go
+++ examples/append.go
@@ -26,17 +26,14 @@ func main() {
 		return
 	}
 
-	switch b.(type) {
-	case string:
-		b = []interface{}{b.(string), strconv.Itoa(2)}
-	case []interface{}:
-		b = append(b.([]interface{}), interface{}(strconv.Itoa(2)))
-	default:
-		// capture map[string]interface{} value
-		fmt.Println("err: invalid type for a.b")
+	b, err = appendElement(b, 2)
+	if err != nil {
+		fmt.Println("append err:", err)
 		return
 	}
 
+	// create the new value for 'b' as a map
+	// and update 'm'
 	val := map[string]interface{}{"b": b}
 	n, err := m.UpdateValuesForPath(val, "a.b")
 	if err != nil {
@@ -50,4 +47,17 @@ func main() {
 
 	x, err := m.XmlIndent("", "  ")
 	fmt.Println(string(x))
+}
+
+func appendElement(v interface{}, n int) (interface{}, error) {
+	switch v.(type) {
+	case string:
+		v = []interface{}{v.(string), strconv.Itoa(n)}
+	case []interface{}:
+		v = append(v.([]interface{}), interface{}(strconv.Itoa(n)))
+	default:
+		// capture map[string]interface{} value, simple element, etc.
+		return v, fmt.Errorf("invalid type")
+	}
+	return v, nil
 }
