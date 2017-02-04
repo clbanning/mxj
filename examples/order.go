7bb6e52c09bb285c5dbe6381ced31777cd8a6709
Charles Banning <clbanning@tamgroupinc.com>
2017-02-04 05:47:40 -0700

    Generalize example and add comments.

--- examples/order.go
+++ examples/order.go
@@ -13,8 +13,10 @@ import (
 var data = `<node>
   <a>sadasd</a>
   <b>gfdfg</b>
+  <c>bcbcbc</c>
   <a>hihihi</a>
   <b>jkjkjk</b>
+  <a>lmlmlm</a>
 </node>`
 
 func main() {
@@ -24,28 +26,40 @@ func main() {
 		return
 	}
 
-	// Merge a and b members into a single list
-	// that we can work with.
+	// Merge a b, and c members into a single list.
+	// The value has "#text" key; "#seq" has the list sequence index.
+	// Preserve both and the list key if you want to reencode.
 	list := make([]*listval, 0)
 	for k, v := range m["node"].(map[string]interface{}) {
-		for _, vv := range v.([]interface{}) {
-			mem := vv.(map[string]interface{})
+		switch v.(type) {
+		case map[string]interface{}:
+			// This handles the lone 'c' element in the list.
+			mem := v.(map[string]interface{})
 			lval := &listval{k, mem["#text"].(string), mem["#seq"].(int)}
 			list = append(list, lval)
+		case []interface{}:
+			// 'a' and 'b' were decoded as slices.
+			for _, vv := range v.([]interface{}) {
+				mem := vv.(map[string]interface{})
+				lval := &listval{k, mem["#text"].(string), mem["#seq"].(int)}
+				list = append(list, lval)
+			}
 		}
 	}
 
-	// sort the list into orignal DOC sequence
+	// Sort the list into orignal DOC sequence.
 	sort.Sort(Lval(list))
 
-	// do some work with the list members - let's swap values
-	for i := 0; i < 2; i++ {
-		list[i].val, list[3-i].val = list[3-i].val, list[i].val
+	// Do some work with the list members - let's swap values.
+	for i := 0; i < 3; i++ {
+		list[i].val, list[5-i].val = list[5-i].val, list[i].val
 	}
 
-	// rebuild map[string]interface{} value for "node"
+	// Rebuild map[string]interface{} value for "node".
+	// Everything can be slice values - []interface{} - for encoding.
 	a := make([]interface{}, 0)
 	b := make([]interface{}, 0)
+	c := make([]interface{}, 0)
 	for _, v := range list {
 		val := map[string]interface{}{"#text": v.val, "#seq": v.seq}
 		switch v.list {
@@ -53,9 +67,11 @@ func main() {
 			a = append(a, interface{}(val))
 		case "b":
 			b = append(b, interface{}(val))
+		case "c":
+			c = append(c, interface{}(val))
 		}
 	}
-	val := map[string]interface{}{"a": a, "b": b}
+	val := map[string]interface{}{"a": a, "b": b, "c": c}
 	m["node"] = interface{}(val)
 
 	x, err := m.XmlSeqIndent("", "  ")
